8473c01f3a27bbdcc2e156a6662e09fa698e19d1
Khutsiso <Khutsisoletageng7@gmail.com>
2026-07-27 11:00:42 +0200

    Register RSVP and GiftClaim models in admin

---
+++ main/admin.py
@@ -0,0 +1,18 @@
+﻿from django.contrib import admin
+from .models import RSVP, GiftClaim
+
+
+@admin.register(RSVP)
+class RSVPAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone_number', 'guest_count', 'attending', 'created_at')
+    list_filter = ('attending', 'created_at')
+    search_fields = ('full_name', 'email', 'phone_number')
+    ordering = ('-created_at',)
+
+
+@admin.register(GiftClaim)
+class GiftClaimAdmin(admin.ModelAdmin):
+    list_display = ('gift_name', 'claimant_name', 'claimant_email', 'created_at')
+    list_filter = ('gift_name', 'created_at')
+    search_fields = ('gift_name', 'claimant_name', 'claimant_email')
+    ordering = ('-created_at',)
